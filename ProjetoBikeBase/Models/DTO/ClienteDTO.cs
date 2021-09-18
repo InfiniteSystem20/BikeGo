98a7b669f77cc57760ad89a3fc38be7503aa46f8
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-09-18 20:39:18 -0300

    Criação das Classes DTO

--- ProjetoBikeBase/Models/DTO/ClienteDTO.cs
+++ ProjetoBikeBase/Models/DTO/ClienteDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,25 @@ namespace ProjetoBikeBase.Models.DTO
 {
     public class ClienteDTO
     {
+        [Display(Name = "Código")]
+        public int IdCliente { get; set; }
+
+        [Display(Name = "Nome")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "CPF")]
+        public string CPFCliente { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string EmailCliente { get; set; }
+
+        [Display(Name = "Telefone")]
+        public string TelCliente { get; set; }
+
+        [Display(Name = "Endereço")]
+        public string EndCliente { get; set; }
+
+        [Display(Name = "Dt. Nascimento")]
+        public string NascCli { get; set; }
     }
 }
---
+++ ProjetoBikeBase/Models/DTO/ProdutoDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBikeBase.Models.DTO
+{
+    public class ProdutoDTO
+    {
+        [Display(Name = "Código")]
+        public int IdProduto { get; set; }
+
+        [Display(Name = "Nome")]
+        public string NomeProd { get; set; }
+
+        [Display(Name = "Quatidade")]
+        public int Qtd { get; set; }
+
+        [Display(Name = "Valor por hora")]
+        public decimal Valor { get; set; }
+
+        [Display(Name = "Status")]
+        public string StatusProd { get; set; }
+    }
+}
