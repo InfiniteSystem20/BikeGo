8c06c72416fd27ab24410aa23fc8ecc6bb61e7f5
Mileni Evelin <1kn.evie@gmail.com>
2021-09-18 21:26:04 -0300

    adc ProdutoDAO e DLL

---
+++ ProjetoBikeBase/Models/DAO/ProdutoDAO.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoBikeBase.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBikeBase.Models.DAO
+{
+    public class ProdutoDAO
+    {
+        String _conexaoMySQL = null;
+        MySqlConnection con = null;
+
+        //String Conexão
+        public ProdutoDAO()
+        {
+            _conexaoMySQL = ConfigurationManager.ConnectionStrings["conexaoMySQL"].ToString();
+        }
+        public void inserirProduto(ProdutoDTO cliente)
+        {
+            try
+            {
+                /*String sql = "insert into tbProduto(NomeProd, Qtd, Valor, StatusProd)"
+                  values (@NomeProd, @Qtd, @Valor, @StatusProd);*/
+
+                String sql = "CALL cadProduto(@NomeProd, @Qtd, @Valor, @StatusProd)";
+                con = new MySqlConnection(_conexaoMySQL);
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@NomeProd", cliente.NomeProd);
+                cmd.Parameters.AddWithValue("@Qtd", cliente.Qtd);
+                cmd.Parameters.AddWithValue("@Valor", cliente.Valor);
+                cmd.Parameters.AddWithValue("@StatusProd", cliente.StatusProd);
+
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+
+                throw new Exception("Erro no banco em cadastro cliente" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Erro na aplicação em cadastro cliente" + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ ProjetoBikeBase/Models/DLL/ProdutoDLL.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoBikeBase.Models.DAO;
+using ProjetoBikeBase.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBikeBase.Models.DLL
+{
+    public class ProdutoDLL
+    {
+        ProdutoDAO dal = null;
+        public ProdutoDLL() { }
+
+        public void novoProduto(ProdutoDTO produtoDto )
+        {
+            try
+            {
+                dal = new ProdutoDAO();
+                dal.inserirProduto(produtoDto);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
