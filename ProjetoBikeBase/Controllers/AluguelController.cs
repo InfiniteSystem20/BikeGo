d9c945ad0dd9ac7daa3dc57f55fd6db9dd090fad
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-09-19 19:08:30 -0300

    Cadastro de Aluguel

---
+++ ProjetoBikeBase/Controllers/AluguelController.cs
@@ -0,0 +1,103 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoBikeBase.Models.DAO;
+using ProjetoBikeBase.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBikeBase.Controllers
+{
+    public class AluguelController : Controller
+    {
+        AluguelDAO aluguelDAO = new AluguelDAO();
+        AluguelDTO aluguelDTO = new AluguelDTO();
+        
+        //CARREGAR CLIENTES
+        public void carregarClientes()
+        {
+            List<SelectListItem> cliente = new List<SelectListItem>();
+
+            using (MySqlConnection con = new MySqlConnection("server=localhost;port=3307;user id=root;password=361190;database=BdBikeCity"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("call SelecionarCliente();", con);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    cliente.Add(new SelectListItem
+                    {
+                        Text = rdr[1].ToString(),
+                        Value = rdr[0].ToString()
+                    });
+                }
+                con.Close();
+
+            }
+
+            ViewBag.Cli = new SelectList(cliente, "Value", "Text");
+        }
+
+        //CARREGAR Produto
+        public void carregarProdutos()
+        {
+            List<SelectListItem> produto = new List<SelectListItem>();
+
+            using (MySqlConnection con = new MySqlConnection("server=localhost;port=3307;user id=root;password=361190;database=BdBikeCity"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("call SelecionarProduto();", con);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    produto.Add(new SelectListItem
+                    {
+                        Text = rdr[1].ToString(),
+                        Value = rdr[0].ToString()
+                    });
+                }
+                con.Close();
+
+            }
+
+            ViewBag.Prod = new SelectList(produto, "Value", "Text");
+        }
+        public ActionResult CadastoAluguel()
+        {
+            carregarClientes();
+            carregarProdutos();
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CadastoAluguel(AluguelDTO aluguelDTO)
+        {
+
+            carregarClientes();
+            carregarProdutos();
+            aluguelDAO.TestarAgenda(aluguelDTO);
+
+            if (aluguelDTO.confAgendamento == "1")
+            {
+                aluguelDTO.IdCliente = Request["Cli"];
+                aluguelDTO.IdProduto = Request["Prod"];
+                aluguelDAO.inserirAluguel(aluguelDTO);
+                ViewBag.msg = "Agendamento realizado com sucesso";
+            }
+            else
+            {
+                ViewBag.msg = "Horário indisponível, por favor escolaher outra data/hora";
+            }
+            return View();
+        }
+
+        // GET: Aluguel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ProjetoBikeBase/Models/DAO/AluguelDAO.cs
+++ ProjetoBikeBase/Models/DAO/AluguelDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ProjetoBikeBase.Models.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,48 @@ namespace ProjetoBikeBase.Models.DAO
 {
     public class AluguelDAO
     {
+        Conexao con = new Conexao();
+        public void TestarAgenda(AluguelDTO agenda)
+        {
+            MySqlCommand cmd = new MySqlCommand("select * from tbAluguel where DataAtend = @DataAtend and HoraAtend = @HoraAtend ", con.MyConectarBD());
+
+            cmd.Parameters.Add("@DataAtend", MySqlDbType.VarChar).Value = agenda.DataAtend;
+            cmd.Parameters.Add("@HoraAtend", MySqlDbType.VarChar).Value = agenda.HoraAtend;
+
+            MySqlDataReader leitor;
+
+            leitor = cmd.ExecuteReader();
+
+            if (leitor.HasRows)
+            {
+                // while (leitor.Read())
+                ///  {
+                agenda.confAgendamento = "0";
+                //   }
+
+            }
+
+            else
+            {
+                agenda.confAgendamento = "1";
+            }
+
+            con.MyDesConectarBD();
+        }
+        public void inserirAluguel(AluguelDTO cm)
+        {
+
+            MySqlCommand cmd = new MySqlCommand("insert into tbAluguel(DataAtend, HoraAtend, HrFinal, ValorTotal, IdCliente, IdProduto)" +
+                                                "values (@DataAtend, @HoraAtend,@HrFinal, @ValorTotal,@IdCliente, @IdProduto)", con.MyConectarBD());
+            cmd.Parameters.Add("@DataAtend", MySqlDbType.VarChar).Value = cm.DataAtend;
+            cmd.Parameters.Add("@HoraAtend", MySqlDbType.VarChar).Value = cm.HoraAtend;
+            cmd.Parameters.Add("@HrFinal", MySqlDbType.VarChar).Value = cm.HrFinal;
+            cmd.Parameters.Add("@ValorTotal", MySqlDbType.VarChar).Value = cm.ValorTotal;
+            cmd.Parameters.Add("@IdCliente", MySqlDbType.VarChar).Value = cm.IdCliente;
+            cmd.Parameters.Add("@IdProduto", MySqlDbType.VarChar).Value = cm.IdProduto;
+
+            cmd.ExecuteNonQuery();
+            con.MyDesConectarBD();
+        }
     }
 }
