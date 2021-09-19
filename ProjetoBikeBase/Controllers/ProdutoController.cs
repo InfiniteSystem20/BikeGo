23827f7e5629fb630e539b977ccdf0a9e75ed071
Mileni Evelin <1kn.evie@gmail.com>
2021-09-18 22:00:35 -0300

    bug produto

---
+++ ProjetoBikeBase/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using ProjetoBikeBase.Models.DLL;
+using ProjetoBikeBase.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBikeBase.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        ProdutoDLL produtoDLL = new ProdutoDLL();
+        ProdutoDTO produtoDTO = new ProdutoDTO();
+
+        public ActionResult CadastroProduto()
+        {
+            return View();
+        }
+
+
+        // CADASTRAR PRODUTO
+        [HttpPost]
+        public ActionResult CadastroProtudo(ProdutoDTO produto)
+        {
+            if (ModelState.IsValid)
+            {
+                produtoDLL.novoProduto(produto);
+
+
+                return RedirectToAction(nameof(CadastroProduto));
+
+                /* ViewBag.msg = "Produto cadastrado com sucesso!";
+                    return View();
+                */
+            }
+            return View();
+        }
+
+    }
+}
