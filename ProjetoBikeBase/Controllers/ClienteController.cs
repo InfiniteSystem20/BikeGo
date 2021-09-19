18c2b760dc8aa51c86c9168d2fac1ee06523a683
Nilson Alcides <nilson_alcides@hotmail.com.com>
2021-09-18 21:36:17 -0300

    Cadastro Cliente

---
+++ ProjetoBikeBase/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using ProjetoBikeBase.Models.DLL;
+using ProjetoBikeBase.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBikeBase.Controllers
+{
+    public class ClienteController : Controller
+    {
+        ClienteDLL clienteDLL = new ClienteDLL();
+        ClienteDTO clienteDTO = new ClienteDTO();
+
+        // CADASTRAR CLIENTE
+        public ActionResult CadastroCliente()
+        {
+            return View();
+        }
+        // CADASTRAR CLIENTE
+        [HttpPost]
+        public ActionResult CadastroCliente(ClienteDTO cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                clienteDLL.novoCliente(cliente);
+
+
+                return RedirectToAction(nameof(CadastroCliente));
+
+                /* ViewBag.msg = "Cliene cadastrado com sucesso!";
+                    return View();
+                */
+            }
+            return View();
+        }
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ProjetoBikeBase/Models/DAO/ClienteDAO.cs
+++ ProjetoBikeBase/Models/DAO/ClienteDAO.cs
@@ -52,5 +52,5 @@ namespace ProjetoBikeBase.Models.DAO
             {
                 con.Close();
             }
-        }
+     }   }
 }
